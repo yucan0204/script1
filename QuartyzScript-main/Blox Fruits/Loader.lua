d82f7816873a4f291da18e270666ac2572c56504
yucan0204 <153425801+yucan0204@users.noreply.github.com>
2025-05-12 22:03:07 +0900

    Update Loader.lua

--- QuartyzScript-main/Blox Fruits/Loader.lua
+++ QuartyzScript-main/Blox Fruits/Loader.lua
@@ -1,5 +1,5 @@
 if Mode == "PVP" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/Blox%20Fruits/Combat.lua"))()
+    loadstring(game:HttpGet("https://github.com/yucan0204/script1/blob/main/QuartyzScript-main/Blox%20Fruits/Combat.lua"))()
 elseif Mode == "OneClick" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yucan0204/script1/refs/heads/main/QuartyzScript-main/Blox%20Fruits/Oneclick.lua"))()
 else

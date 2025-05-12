9f907526df4ee1326127b07e70ef4c5097387969
yucan0204 <153425801+yucan0204@users.noreply.github.com>
2025-05-12 21:53:23 +0900

    Delete Blox Fruits directory

--- Blox Fruits/Loader.lua
+++
@@ -1,7 +0,0 @@
-if Mode == "PVP" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yucan0204/script1/refs/heads/main/Blox%20Fruits/Combat.lua"))()
-elseif Mode == "OneClick" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yucan0204/script1/refs/heads/main/Blox%20Fruits/Combat.lua"))()
-else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yucan0204/script1/refs/heads/main/Blox%20Fruits/Default.lua"))()
-end
